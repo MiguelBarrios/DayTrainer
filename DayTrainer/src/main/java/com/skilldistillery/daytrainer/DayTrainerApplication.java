0df5d531fafad63168792e88e53c4e51f752f153
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-05-24 12:00:44 -0500

    Create and configure Spring Boot, add JPA project dependency

---
+++ DayTrainer/src/main/java/com/skilldistillery/daytrainer/DayTrainerApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.daytrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayTrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayTrainerApplication.class, args);
+	}
+
+}
---
+++ DayTrainer/src/test/java/com/skilldistillery/daytrainer/DayTrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.daytrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayTrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
