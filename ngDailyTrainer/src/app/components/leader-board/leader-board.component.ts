6b182eb2f4b21ed8dd28e5851d4b51fcdcb7cdaf
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-05-25 12:44:26 -0500

    completed layout for leaderboard component

--- ngDailyTrainer/src/app/components/leader-board/leader-board.component.ts
+++ ngDailyTrainer/src/app/components/leader-board/leader-board.component.ts
@@ -8,17 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeaderBoardComponent implements OnInit {
 
+
+
   topWinners:Contestant[]  = [];
   topLosers:Contestant[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
-
+    this.getTopWinners();
   }
 
   getTopWinners(){
+    // tmp
+    this.topWinners = [
+      new Contestant("Winner A", 3700, 312.0),
+      new Contestant("Winner B", 2300, 121.2),
+      new Contestant("Winner C", 230, 89.5),
+      new Contestant("Winner D", 12, 21.3),
+    ]
 
+    this.topLosers = [
+      new Contestant("Loser A", -1230, -67.3),
+      new Contestant("Loser B",434.56, -89.1),
+      new Contestant("Loser C",-1232, -92.3),
+      new Contestant("Loser D",-54.34, -99.91),
+    ]
   }
+
 
 }
--- ngDailyTrainer/src/app/models/contestant.ts
+++ ngDailyTrainer/src/app/models/contestant.ts
@@ -1,16 +1,19 @@
 export class Contestant {
   username:string | null;
   totalWinnings: number | null;
+  percentChange: number | null;
   accountValue: number | null;
   profilePic: string | null;
   numberOfTrades: number | null;
   constructor(username: string | null = "",
     totalWinnings: number | null = 0,
+    percentChange: number | null =0,
     accountValue: number | null = 0,
     profilePic: string | null = "",
     numberOfTrades: number | null = 0){
       this.username = username;
       this.totalWinnings = totalWinnings;
+      this.percentChange = percentChange;
       this.accountValue = accountValue;
       this.profilePic = profilePic;
       this.numberOfTrades = numberOfTrades;
