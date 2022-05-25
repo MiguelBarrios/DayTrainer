8dcb75117bf456ba378c3ec9eb71bd9089f89b88
Daniel.Kregstein <Akregstein@hotmail.com>
2022-05-25 14:10:31 -0600

    fixed one to one mapping

--- DayTrainer/src/main/java/com/skilldistillery/daytrainer/controllers/UserController.java
+++ DayTrainer/src/main/java/com/skilldistillery/daytrainer/controllers/UserController.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.daytrainer.entities.User;
 import com.skilldistillery.daytrainer.services.UserService;
 
 @RestController
+@RequestMapping("api")
 @CrossOrigin({"*", "http://localhost"})
 public class UserController {
 	
