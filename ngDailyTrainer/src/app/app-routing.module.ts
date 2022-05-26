e05cadc8159852e0a8d33fbafc746748f32a42b0
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-05-26 18:28:38 -0500

    Stubbed component routing and navbar

--- ngDailyTrainer/src/app/app-routing.module.ts
+++ ngDailyTrainer/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { LeaderBoardComponent } from './components/leader-board/leader-board.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 import { SingleStockViewComponent } from './components/single-stock-view/single-stock-view.component';
 import { TopMoversComponent } from './components/top-movers/top-movers.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // { path: '/home', component: HomeComponent}
+  // { path: 'leaderBoard', LeaderBoardComponent}
   { path: 'topMovers', component: TopMoversComponent},
-  { path: 'singleStockView', component: SingleStockViewComponent}
+  { path: 'singleStockView', component: SingleStockViewComponent},
+  { path: 'create-user', component: CreateUserComponent},
+
 
 ];
 
