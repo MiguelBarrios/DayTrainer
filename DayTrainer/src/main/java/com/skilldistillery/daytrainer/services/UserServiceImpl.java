99dce3ec65defd15b601e3c20e57379b54115931
Daniel.Kregstein <Akregstein@hotmail.com>
2022-05-26 09:40:17 -0600

    user routing

--- DayTrainer/src/main/java/com/skilldistillery/daytrainer/services/UserServiceImpl.java
+++ DayTrainer/src/main/java/com/skilldistillery/daytrainer/services/UserServiceImpl.java
@@ -10,17 +10,41 @@ import com.skilldistillery.daytrainer.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	private UserRepository userRepo;
 
 	@Override
-	public User getUserById(int userId) {
-		Optional<User> userOpt = userRepo.findById(userId);
-		if(userOpt.isPresent()) {
-			return userOpt.get();
+	public User getUserById(int userId, String name) {
+		User currentUser = userRepo.findByUsername(name);
+		if (currentUser != null && currentUser.getRole().equals("admin")) {
+			Optional<User> found = userRepo.findById(userId);
+			User foundUser = found.get();
+			if (foundUser != null) {
+				return foundUser;
+
+			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public User update(String name, User user) {
+		User currentUser = userRepo.findByUsername(name);
+		if (currentUser != null && currentUser.getRole().equals("admin")) {
+			return userRepo.save(user);
 		}
 		return null;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public void destroy(String name, int userId) {
+		User currentUser = userRepo.findByUsername(name);
+		if (currentUser != null && currentUser.getRole().equals("admin")) {
+			userRepo.delete(userRepo.getById(userId));
+		}
 	}
 
 }
