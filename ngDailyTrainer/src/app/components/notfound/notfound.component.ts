4fc249d62e2b76c3a3d0909eeee98f302ce05659
Daniel.Kregstein <Akregstein@hotmail.com>
2022-05-30 12:09:19 -0600

    page not found redirect

--- ngDailyTrainer/src/app/components/notfound/notfound.component.ts
+++ ngDailyTrainer/src/app/components/notfound/notfound.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { timeout } from 'rxjs';
+
 
 @Component({
   selector: 'app-notfound',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotfoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.countdown();
   }
+countdown(){
+  let time:string[]=["9","8","7","6","5","4","3","2","1"]
+  setTimeout(() => {
+    this.router.navigateByUrl('home')
+  }, 10000);
+
+ setTimeout(() => {
+   let count = document.getElementById('count')
+   if(count?.textContent != null){
+    let counter = parseInt(count.textContent)
+  counter -= 1
+  console.log(counter);
+
+   }
+
+ }, 1000);
+}
 
 }
