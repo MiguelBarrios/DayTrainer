ea1b48272abc2f185dcafd1d21256e159a62507b
Cecelia Guerrero <whit3fenc3@gmail.com>
2022-06-01 15:54:40 -0500

    removed unused imports

--- DayTrainer/src/main/java/com/skilldistillery/daytrainer/repository/UserRepository.java
+++ DayTrainer/src/main/java/com/skilldistillery/daytrainer/repository/UserRepository.java
@@ -1,10 +1,6 @@
 package com.skilldistillery.daytrainer.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.skilldistillery.daytrainer.entities.User;
 
