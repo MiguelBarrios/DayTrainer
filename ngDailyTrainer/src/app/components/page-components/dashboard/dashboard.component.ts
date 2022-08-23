778d7c000b9f068311c36c70238f748bade746a6
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-08-23 16:05:27 -0500

    added auto routing to user dashboard if logged in

--- ngDailyTrainer/src/app/components/page-components/dashboard/dashboard.component.ts
+++ ngDailyTrainer/src/app/components/page-components/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService } from 'src/app/services/account.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { TradesService } from 'src/app/services/trades.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +12,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService,
+    private router:Router,
+    private userServ:UsersService, 
+    private tradeServ:TradesService,
+    private accountService:AccountService) { }
 
   ngOnInit(): void {
+    if(!this.auth.checkLogin()){
+      this.router.navigateByUrl('home');
+    }
   }
 
 }
