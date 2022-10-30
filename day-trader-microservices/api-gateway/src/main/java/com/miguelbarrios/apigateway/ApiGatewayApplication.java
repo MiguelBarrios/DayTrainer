a566cd5f60c7326b140ff4966edf19fa4c0631f4
MiguelBarrios <miguelbarriosdavila@gmail.com>
2022-10-29 21:31:47 -0500

    finished api-gateway

--- day-trader-microservices/api-gateway/src/main/java/com/miguelbarrios/apigateway/ApiGatewayApplication.java
+++ day-trader-microservices/api-gateway/src/main/java/com/miguelbarrios/apigateway/ApiGatewayApplication.java
@@ -1,2 +1,13 @@
-package com.miguelbarrios.apigateway;public class ApiGatewayApplication {
+package com.miguelbarrios.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
