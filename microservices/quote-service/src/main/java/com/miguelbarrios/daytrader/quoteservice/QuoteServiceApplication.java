9291d9086b196719396d95f64fd98a8e6e238be7
MiguelBarrios <miguelbarriosdavila@gmail.com>
2023-01-15 22:40:05 -0600

    created microservices project

---
+++ microservices/quote-service/src/main/java/com/miguelbarrios/daytrader/quoteservice/QuoteServiceApplication.java
@@ -0,0 +1,31 @@
+package com.miguelbarrios.daytrader.quoteservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+public class QuoteServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteServiceApplication.class, args);
+    }
+
+//    @Bean
+//    JedisConnectionFactory jedisConnectionFactory() {
+//        JedisConnectionFactory jedisConFactory
+//                = new JedisConnectionFactory();
+//        jedisConFactory.setHostName("localhost");
+//        jedisConFactory.setPort(6379);
+//        return jedisConFactory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        RedisTemplate<String, Object> template = new RedisTemplate<>();
+//        template.setConnectionFactory(jedisConnectionFactory());
+//        return template;
+//    }
+}
