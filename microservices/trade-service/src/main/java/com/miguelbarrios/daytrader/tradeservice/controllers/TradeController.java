78f73a30dd35a31be12cf0aca6e588086fa564be
MiguelBarrios <miguelbarriosdavila@gmail.com>
2023-01-19 18:50:00 -0600

    created and configured trade microservice

---
+++ microservices/trade-service/src/main/java/com/miguelbarrios/daytrader/tradeservice/controllers/TradeController.java
@@ -0,0 +1,17 @@
+package com.miguelbarrios.daytrader.tradeservice.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/trade")
+@CrossOrigin({ "*", "http://localhost" })
+public class TradeController {
+
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
+}
