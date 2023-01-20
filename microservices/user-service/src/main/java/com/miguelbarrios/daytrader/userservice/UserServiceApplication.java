a3f6c30d415a4787351ac1942ec606758e27e365
MiguelBarrios <miguelbarriosdavila@gmail.com>
2023-01-19 19:30:44 -0600

    removed docker mysql files, added user-service routes to api gateway

--- microservices/user-service/src/main/java/com/miguelbarrios/daytrader/userservice/UserServiceApplication.java
+++ microservices/user-service/src/main/java/com/miguelbarrios/daytrader/userservice/UserServiceApplication.java
@@ -20,30 +20,30 @@ public class UserServiceApplication {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner run(UserService userService){
-		// Evertying inside the {} will run after application startup
-		return args -> {
-			userService.saveRole(new Role(null, "ROLE_USER"));
-			userService.saveRole(new Role(null, "ROLE_MANAGER"));
-			userService.saveRole(new Role(null, "ROLE_ADMIN"));
-			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
-
-			userService.saveUser(new AppUser(null, "John Trovolta", "john", "password", new ArrayList<>()));
-			userService.saveUser(new AppUser(null, "Will Smith", "will", "password", new ArrayList<>()));
-			userService.saveUser(new AppUser(null, "Jim Cary", "jim", "password", new ArrayList<>()));
-			userService.saveUser(new AppUser(null, "Arnold Schwarzenegger", "arnold", "password", new ArrayList<>()));
-
-			userService.addRoleToUser("john", "ROLE_USER");
-			userService.addRoleToUser("john", "ROLE_MANAGER");
-
-			userService.addRoleToUser("will", "ROLE_MANAGER");
-			userService.addRoleToUser("jim", "ROLE_ADMIN");
-			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
-			userService.addRoleToUser("arnold", "ROLE_ADMIN");
-			userService.addRoleToUser("arnold", "ROLE_USER");
-		};
-	}
+//	@Bean
+//	CommandLineRunner run(UserService userService){
+//		// Evertying inside the {} will run after application startup
+//		return args -> {
+//			userService.saveRole(new Role(null, "ROLE_USER"));
+//			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+//			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+//
+//			userService.saveUser(new AppUser(null, "John Trovolta", "john", "password", new ArrayList<>()));
+//			userService.saveUser(new AppUser(null, "Will Smith", "will", "password", new ArrayList<>()));
+//			userService.saveUser(new AppUser(null, "Jim Cary", "jim", "password", new ArrayList<>()));
+//			userService.saveUser(new AppUser(null, "Arnold Schwarzenegger", "arnold", "password", new ArrayList<>()));
+//
+//			userService.addRoleToUser("john", "ROLE_USER");
+//			userService.addRoleToUser("john", "ROLE_MANAGER");
+//
+//			userService.addRoleToUser("will", "ROLE_MANAGER");
+//			userService.addRoleToUser("jim", "ROLE_ADMIN");
+//			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+//			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+//			userService.addRoleToUser("arnold", "ROLE_USER");
+//		};
+//	}
 
 	@Bean
 	PasswordEncoder passwordEncoder(){
